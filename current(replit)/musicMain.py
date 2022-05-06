dd72d6ce65292c889786658f3de1738cc76628a9
MiguelV5 <mvasquezj@fi.uba.ar>
2022-05-06 02:43:16 -0300

    hid encoded strs

--- current(replit)/musicMain.py
+++ current(replit)/musicMain.py
@@ -1,22 +1,24 @@
 import os
 import discord
-from discord.ext import commands 
+from discord.ext import commands
 import musicHandler
 
 cogs = [musicHandler]
 
-client = commands.Bot(command_prefix = '?', intents = discord.Intents.all(), description = 'Bot chancludo de miguel')
+client = commands.Bot(command_prefix='?', intents=discord.Intents.all(
+), description='Bot chancludo de miguel')
 
 for i in range(len(cogs)):
-  cogs[i].setup(client)
+    cogs[i].setup(client)
+
 
 @client.event
 async def on_ready():
-  await client.change_presence(activity = discord.Game(name="Bot chancludo que pone musica"))
-  print('Bot is ready to work.')
+    await client.change_presence(activity=discord.Game(name="Bot chancludo que pone musica"))
+    print('Bot is ready to work.')
 
-#@client.event
-#async def on_voice_state_update():
+# @client.event
+# async def on_voice_state_update():
 #  await client.change_presence(activity = discord.Game(name="Bot chancludo que pone musica"))
 #  print('Bot is ready to work.')
 
